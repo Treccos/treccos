843a67971bc034093363808388fad66cb9221e72
HenriqueCarvalho <henrique.o.carvalho@hotmail.com.br>
2016-09-20 03:08:13 -0300

    Revert "Revert "Added validations to task model""
    This reverts commit 9cabe2ed2b14e8d303acb66b809eebe8a522ab61.

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,11 +2,14 @@ class Task < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :project
 
-	 auto_html_for :content do
-	 	html_escape
-	 	image
-	 	youtube(:width => "100%", :height => 250, :autoplay => false)
-	 	link :target => "_blank", :rel => "nofollow"
-	 	simple_format
-	 end
+	validates :user_id, presence: true
+	validates :content, presence: true
+
+	auto_html_for :content do
+		html_escape
+		image
+		youtube(:width => "100%", :height => 250, :autoplay => false)
+		link :target => "_blank", :rel => "nofollow"
+		simple_format
+	end
 end
